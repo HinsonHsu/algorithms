220f77868d9fec95d6911cc5ac75ba05e6eb7da6
shuxinsheng <shuxinsheng@qiyi.com>
2022-07-27 21:23:25 +0800

    归并排序

---
+++ src/main/java/org/hinson/sort/MergeSort.java
@@ -0,0 +1,36 @@
+package org.hinson.sort;
+
+import java.util.Arrays;
+
+/**
+ * 归并排序，空间复杂度O(N)
+ * created on 2022-07-27
+ */
+public class MergeSort {
+
+    public int[] sort(int[] nums) {
+        if (nums.length < 2) {
+            return nums;
+        }
+        int mid = nums.length / 2;
+        int[] left = Arrays.copyOfRange(nums, 0, mid);
+        int[] right = Arrays.copyOfRange(nums, mid, nums.length);
+
+        int[] arr = new int[left.length + right.length];
+        int m = 0, i = 0, j = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                arr[m++] = left[i++];
+            } else {
+                arr[m++] = right[j++];
+            }
+        }
+        while (i < left.length) {
+            arr[m++] = left[i++];
+        }
+        while (j < right.length) {
+            arr[m++] = right[j++];
+        }
+        return arr;
+    }
+}
