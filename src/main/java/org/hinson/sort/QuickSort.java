3dffac25e7714cf0b2e32c65bc25e9e46729cb99
shuxinsheng <shuxinsheng@qiyi.com>
2022-07-27 21:31:03 +0800

    快速排序

---
+++ src/main/java/org/hinson/sort/QuickSort.java
@@ -0,0 +1,43 @@
+package org.hinson.sort;
+
+/**
+ * 快速排序
+ * created on 2022-07-27
+ */
+public class QuickSort {
+
+    public int[] sort(int[] nums) {
+        quickSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    public void quickSort(int[] nums, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int tmp = nums[start];
+        int left = start + 1;
+        int right = end;
+        while (left != right) {
+            while (left < right && nums[right] >= tmp) {
+                right--;
+            }
+            while (left < right && nums[left] <= tmp) {
+                left++;
+            }
+            if (left < right) {
+                swap(nums, left, right);
+            }
+        }
+        nums[start] = nums[left];
+        nums[left] = tmp;
+        quickSort(nums, start, left - 1);
+        quickSort(nums, left + 1, end);
+    }
+
+    private void swap(int[] nums, int left, int right) {
+        int tmp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = tmp;
+    }
+}
