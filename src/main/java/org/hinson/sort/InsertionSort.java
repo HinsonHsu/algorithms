a6db2c38bcf6534706ad639798a5b54b8b392b3f
shuxinsheng <shuxinsheng@qiyi.com>
2022-07-27 21:03:58 +0800

    插入排序

---
+++ src/main/java/org/hinson/sort/InsertionSort.java
@@ -0,0 +1,27 @@
+package org.hinson.sort;
+
+import java.util.Arrays;
+
+/**
+ * 插入排序
+ * created on 2022-07-27
+ */
+public class InsertionSort {
+
+    public int[] sort(int[] nums) {
+        int[] arr = Arrays.copyOf(nums, nums.length);
+
+        for (int i = 1; i < arr.length; i++) {
+            int tmp = arr[i];
+            int j = i;
+            while (j > 0 && tmp < arr[j - 1]) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            if (j != i) {
+                arr[j] = tmp;
+            }
+        }
+        return arr;
+    }
+}
