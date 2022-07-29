12dd7bcec44de51789e0535cf312ab02c643e022
shuxinsheng <shuxinsheng@qiyi.com>
2022-07-29 11:06:17 +0800

    桶排序 & 基数排序

---
+++ src/main/java/org/hinson/sort/BucketSort.java
@@ -0,0 +1,81 @@
+package org.hinson.sort;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class BucketSort {
+
+    public int[] sort(int[] nums) {
+        int[] arr = Arrays.copyOf(nums, nums.length);
+        return bucketSort(arr, 5);
+    }
+
+    private int[] bucketSort(int[] arr, int bucketSize) {
+        if (arr.length == 0) {
+            return arr;
+        }
+
+        int minValue = arr[0];
+        int maxValue = arr[1];
+        for (int value : arr) {
+            if (value < minValue) {
+                minValue = value;
+            } else if (value > maxValue) {
+                maxValue = value;
+            }
+        }
+
+        int bucketCount = (maxValue - minValue) / bucketSize + 1;
+        int[][] buckets = new int[bucketCount][0];
+
+        for (int i = 0; i < arr.length; i++) {
+            int index = (arr[i] - minValue) / bucketSize;
+            buckets[index] = arrAppend(buckets[index], arr[i]);
+        }
+
+        int arrIndex = 0;
+        for (int[] bucket : buckets) {
+            if (bucket.length <= 0) {
+                continue;
+            }
+            Arrays.sort(bucket);
+            for (int value : bucket) {
+                arr[arrIndex++] = value;
+            }
+        }
+        return arr;
+    }
+
+    private int[] arrAppend(int[] arr, int value) {
+        arr = Arrays.copyOf(arr, arr.length + 1);
+        arr[arr.length - 1] = value;
+        // 插入排序
+        int j = arr.length - 1;
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            swap(arr, j - 1, j);
+        }
+
+        return arr;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[100];
+        Random random = new Random();
+        for (int i = 0; i < nums.length; i++) {
+
+            nums[i] = random.nextInt(100);
+        }
+        System.out.println(Arrays.toString(nums));
+
+        BucketSort bucketSort = new BucketSort();
+        int[] newArr = bucketSort.sort(nums);
+        System.out.println(Arrays.toString(newArr));
+
+    }
+}
---
+++ src/main/java/org/hinson/sort/RadixSort.java
@@ -0,0 +1,66 @@
+package org.hinson.sort;
+
+import java.util.Arrays;
+
+public class RadixSort {
+
+    public int[] sort(int[] nums) {
+        int[] arr = Arrays.copyOf(nums, nums.length);
+
+        int maxDigit = getMaxDigit(arr);
+
+        return radixSort(arr, maxDigit);
+    }
+
+    private int getMaxDigit(int[] arr) {
+        int maxValue = getMaxValue(arr);
+        return getNumLength(maxValue);
+    }
+
+    private int getMaxValue(int[] arr) {
+        int maxValue = arr[0];
+        for (int value : arr) {
+            if (maxValue < value) {
+                maxValue = value;
+            }
+        }
+        return maxValue;
+    }
+
+    private int getNumLength(long num) {
+        if (num == 0) {
+            return 1;
+        }
+        int length = 0;
+        for (long tmp = num; tmp != 0; tmp /= 10) {
+            length++;
+        }
+        return length;
+    }
+
+    private int[] radixSort(int[] arr, int maxDigit) {
+        int mod = 10;
+        int dev = 1;
+        for (int i = 0; i < maxDigit; i++, dev *= 10, mod *= 10) {
+            int[][] counter = new int[mod * 2][0];
+
+            for (int k : arr) {
+                int bucket = ((k % mod) / dev) + mod;
+                counter[bucket] = arrayAppend(counter[bucket], k);
+            }
+            int pos = 0;
+            for (int[] bucket : counter) {
+                for (int value : bucket) {
+                    arr[pos++] = value;
+                }
+            }
+        }
+        return arr;
+    }
+
+    private int[] arrayAppend(int[] arr, int value) {
+        arr = Arrays.copyOf(arr, arr.length + 1);
+        arr[arr.length - 1] = value;
+        return arr;
+    }
+}
