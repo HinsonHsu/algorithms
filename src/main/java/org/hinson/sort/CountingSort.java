705f09803b669a218ab748d3426b196bbcc6ea01
shuxinsheng <shuxinsheng@qiyi.com>
2022-07-29 09:57:46 +0800

    add commit

---
+++ src/main/java/org/hinson/sort/CountingSort.java
@@ -0,0 +1,45 @@
+package org.hinson.sort;
+
+import java.util.Arrays;
+
+/**
+ * created on 2022-07-29
+ * 计数排序
+ */
+public class CountingSort {
+
+    public int[] sort(int[] nums) {
+        int[] arr = Arrays.copyOf(nums, nums.length);
+
+        int maxValue = getMaxValue(nums);
+
+        return countingSort(arr, maxValue);
+    }
+
+    private int getMaxValue(int[] nums) {
+        int maxValue = nums[0];
+        for (int value : nums) {
+            if (maxValue < value) {
+                maxValue = value;
+            }
+        }
+        return maxValue;
+    }
+
+    private int[] countingSort(int[] arr, int maxValue) {
+        int bucketLen = maxValue + 1;
+        int[] bucket = new int[bucketLen];
+
+        for (int value : arr) {
+            bucket[value]++;
+        }
+        int sortedIndex = 0;
+        for (int j = 0; j < bucketLen; j++) {
+            while (bucket[j] > 0) {
+                arr[sortedIndex++] = j;
+                bucket[j]--;
+            }
+        }
+        return arr;
+    }
+}
---
+++ src/main/java/org/hinson/sort/HeapSort.java
@@ -0,0 +1,55 @@
+package org.hinson.sort;
+
+import java.util.Arrays;
+
+/**
+ * created on 2022-07-29
+ * 堆排序
+ */
+public class HeapSort {
+
+    public int[] sort(int[] nums) {
+        int[] arr = Arrays.copyOf(nums, nums.length);
+
+        int len = arr.length;
+
+        buildMaxHeap(arr);
+
+        for (int i = len - 1; i > 0; i--) {
+            swap(arr, i, 0);
+            len--;
+            heapify(arr, i, len);
+        }
+
+        return arr;
+    }
+
+    private void buildMaxHeap(int[] arr) {
+        int len = arr.length;
+        for (int i = (len / 2); i >= 0; i--) {
+            heapify(arr, i, len);
+        }
+    }
+
+    private void heapify(int[] arr, int i, int len) {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int largest = i;
+        if (left < len && arr[left] > arr[i]) {
+            largest = left;
+        }
+        if (right < len && arr[right] > arr[i]) {
+            largest = right;
+        }
+        if (largest != i) {
+            swap(arr, largest, i);
+            heapify(arr, largest, len);
+        }
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
