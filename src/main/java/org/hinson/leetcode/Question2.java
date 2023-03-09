5733e64a8cf2f59a8a22d3f54dd572ef1443c30e
shuxinsheng <shuxinsheng@qiyi.com>
2023-03-09 23:25:58 +0800

    q2

--- src/main/java/org/hinson/leetcode/Question2.java
+++ src/main/java/org/hinson/leetcode/Question2.java
@@ -12,41 +12,25 @@ public class Question2 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int carry = 0;
         ListNode dummyHead = new ListNode();
-        ListNode head = dummyHead;
-        while (l1 != null && l2 != null) {
-            int sum = carry + l1.val + l2.val;
-            if (sum >= 10) {
-                sum %= 10;
-                carry = 1;
-            } else {
-                carry = 0;
-            }
-            head.next = new ListNode(sum);
-            head = head.next;
-            l1 = l1.next;
-            l2 = l2.next;
-        }
-        while (l1 != null || l2 != null) {
-            int sum = carry;
+        ListNode cursor = dummyHead;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int l1Val = l1 != null ? l1.val : 0;
+            int l2Val = l2 != null ? l2.val : 0;
+            int sumVal = l1Val + l2Val + carry;
+
+            carry = sumVal / 10;
+
+            ListNode sumNode = new ListNode(sumVal % 10);
+            cursor.next = sumNode;
+            cursor = sumNode;
+
             if (l1 != null) {
-                sum += l1.val;
                 l1 = l1.next;
             }
             if (l2 != null) {
-                sum += l2.val;
                 l2 = l2.next;
             }
-            if (sum >= 10) {
-                sum %= 10;
-                carry = 1;
-            } else {
-                carry = 0;
-            }
-            head.next = new ListNode(sum);
-            head = head.next;
-        }
-        if (carry > 0) {
-            head.next = new ListNode(carry);
         }
         return dummyHead.next;
     }
