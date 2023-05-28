9b0369c4dca99ac3da805db281de304f360edbfe
shuxinsheng <shuxinsheng@qiyi.com>
2023-05-29 07:36:35 +0800

    add 10

---
+++ src/main/java/org/hinson/leetcode/Question6.java
@@ -0,0 +1,31 @@
+package org.hinson.leetcode;
+
+public class Question6 {
+
+    public String convert(String s, int numRows) {
+        int sLen = s.length();
+        StringBuilder sb = new StringBuilder();
+        int circle = 2 * numRows - 2;
+        if (circle == 0 || circle < sLen) {
+            return s;
+        }
+        for (int i = 0; i < numRows; i++) {
+            int start1 = i;
+            while (start1 < sLen) {
+                sb.append(s.charAt(start1));
+                int end1 =  start1 - (start1 % circle) + circle - i;
+                if (i != 0 && i != numRows - 1 && end1 < sLen) {
+                    sb.append(s.charAt(end1));
+                }
+                start1 += circle;
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "ABCDE";
+        String res = new Question6().convert(s, 4);
+        System.out.println(res);
+    }
+}
