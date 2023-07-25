e16e006b5be5fb5cf0fea95d3e43c64fa3bdc623
shuxinsheng <shuxinsheng@qiyi.com>
2023-07-25 22:07:10 +0800

    1

--- src/main/java/org/hinson/leetcode/Question1.java
+++ src/main/java/org/hinson/leetcode/Question1.java
@@ -10,6 +10,7 @@ import java.util.Map;
 public class Question1 {
 
     public int[] twoSum(int[] nums, int target) {
+
         Map<Integer, Integer> hash = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (hash.containsKey(target - nums[i])) {
