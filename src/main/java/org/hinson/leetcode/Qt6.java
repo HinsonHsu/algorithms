384409746de7999aedef5f8a580e66fd48796408
shuxinsheng <shuxinsheng@qiyi.com>
2023-10-17 21:18:58 +0800

    qt6

--- src/main/java/org/hinson/leetcode/Qt6.java
+++ src/main/java/org/hinson/leetcode/Qt6.java
@@ -6,14 +6,14 @@ public class Qt6 {
         int sLen = s.length();
         StringBuilder sb = new StringBuilder();
         int circle = 2 * numRows - 2;
-        if (circle == 0 || circle < sLen) {
+        if (circle == 0) {
             return s;
         }
         for (int i = 0; i < numRows; i++) {
             int start1 = i;
             while (start1 < sLen) {
                 sb.append(s.charAt(start1));
-                int end1 =  start1 - (start1 % circle) + circle - i;
+                int end1 = start1 - i + (circle - i);
                 if (i != 0 && i != numRows - 1 && end1 < sLen) {
                     sb.append(s.charAt(end1));
                 }
@@ -24,8 +24,8 @@ public class Qt6 {
     }
 
     public static void main(String[] args) {
-        String s = "ABCDE";
-        String res = new Qt6().convert(s, 4);
+        String s = "PAYPALISHIRING";
+        String res = new Qt6().convert(s, 3);
         System.out.println(res);
     }
 }
