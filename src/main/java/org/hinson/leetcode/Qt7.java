a1c10738e007e1f6c03838439a72895c9fca688c
shuxinsheng <shuxinsheng@qiyi.com>
2023-10-18 17:21:20 +0800

    qt7

---
+++ src/main/java/org/hinson/leetcode/Qt7.java
@@ -0,0 +1,31 @@
+package org.hinson.leetcode;
+
+public class Qt7 {
+
+    public int reverse(int x) {
+        int res = 0;
+        while (x != 0) {
+            //每次取末尾数字
+            int tmp = x % 10;
+            //判断是否 大于 最大32位整数
+            if (res > 214748364 || (res == 214748364 && tmp > 7)) {
+                return 0;
+            }
+            //判断是否 小于 最小32位整数
+            if (res < -214748364 || (res == -214748364 && tmp < -8)) {
+                return 0;
+            }
+            res = res * 10 + tmp;
+            x /= 10;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        int x = -100;
+        Object res = new Qt7().reverse(x);
+        System.out.println(res);
+    }
+}
