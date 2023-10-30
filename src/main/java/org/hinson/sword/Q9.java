6ca8bcf3bfe80f6ba8d25f5d9a5121e2f9ff89d1
shuxinsheng <shuxinsheng@qiyi.com>
2023-10-30 20:54:37 +0800

    双栈实现队列

---
+++ src/main/java/org/hinson/sword/Q9.java
@@ -0,0 +1,29 @@
+package org.hinson.sword;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Q9 {
+
+    public Deque<Integer> in = new LinkedList<>();
+
+    public Deque<Integer> out = new LinkedList<>();
+
+
+    public void push(int node) {
+        in.push(node);
+    }
+
+    public int pop() throws Exception {
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
+                out.push(in.pop());
+            }
+        }
+        if (out.isEmpty()) {
+            throw new Exception("queue is empty!");
+        }
+        return out.pop();
+    }
+    
+}
